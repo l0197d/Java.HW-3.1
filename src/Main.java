7828ac578018a5d52cdc0755a2b1ce056e267767
l0197d <titus04@yandex.ru>
2021-01-21 20:30:44 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 36000;
+        int miles = 20;
+        int bonus = ticket / miles;
+        System.out.print(bonus);
+    }
+
+
+
+
+}
